6849e8fb5db7f34938a49278587dc7185241b188
NeemaMwende <gracepete12@gmail.com>
2024-06-09 21:18:54 +0300

    updates

--- src/components/Bio.jsx
+++ src/components/Bio.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
 import "./Bio.css";
-import mark from "../Images/mark2.jpeg"
-// import Link from 'react-router-dom';
+import peter from "../Images/pastors.jpg";
 
 const Bio = () => {
   return (
-        <section class="home" id="home">
-          {/* <Link to="/">Home</Link> */}
-          <div className='bio'>
-              <div className="content animate___animated animate_fadeIn animate_infinite">
-                    <h3>Welcome to <span>Deliverance Church</span> </h3>
-                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-                        Nulla iure ad quos vero nam culpa, minima ipsum nemo, 
-                        natus fugit, quas a? Architecto ut omnis accusamus libero neque asperiores impedit.</p>
-                        <div class="button">Join Us Today</div>
-                </div>
-                <div className='profile-img'>
-                  <img src={mark} alt="" />
-                </div>
-          </div>
-            
-          
-        </section>
-  )
-}
+    <section className="home" id="home">
+      <div className="bio">
+        <div className="content animate___animated animate_fadeIn animate_infinite">
+          <h3>Welcome to <span>Deliverance Church</span></h3>
+          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla iure ad quos vero nam culpa, minima ipsum nemo, natus fugit, quas a? Architecto ut omnis accusamus libero neque asperiores impedit.</p>
+          <div className="button">Join Us Today</div>
+        </div>
+        <div className="profile-img">
+          <img src={peter} alt="Pastor" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Bio;
